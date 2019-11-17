0dbc25d903d8ed76e4c27449e6c49960d3843ff6
Freitacr <freitacr@plu.edu>
2019-11-16 23:24:51 -0800

    Implement Basic Archive Webpages

---
+++ public/archiveresults.js
@@ -0,0 +1,150 @@
+let queryResultsTableInitialHtml = undefined;
+
+function retrieveComplaintGroupResults(loginCookie) {
+    let cookieStr = decodeURIComponent(document.cookie);
+    let reqBody = undefined;
+    cookieStr.split(';').forEach(function(cookie) {
+        if(cookie.trim().startsWith("OMISA_WEB_COOKIE"))
+            reqBody = cookie.split('=')[1];
+    })
+    if(reqBody == undefined) {
+        window.location.replace("newaquery.html");
+        return;
+    }
+    let bodyObj = JSON.parse(reqBody);
+
+    let reqObj = {
+        Entry: {
+            Make: bodyObj.make.toLowerCase(),
+            Model: bodyObj.model.toLowerCase(),
+            Problem: bodyObj.problem.toLowerCase()
+        },
+        UserId: loginCookie.userId,
+        LoginToken: loginCookie.token,
+        CompanyId: parseInt(bodyObj.companyId)
+    };
+
+    let fetchInit = {
+        method: "PUT",
+        body: JSON.stringify(reqObj)
+    };
+    fetch("https://jcf-ai.com:16384/archive", fetchInit).then(response=> {
+        if(response.status == 200) {
+            response.text().then(value=> {
+                let complaintGroupTable = document.getElementById('problem-group-table');
+                let respJson = JSON.parse(value);
+                respJson.forEach(complaintGroupJson => {
+                    complaintGroupTable.innerHTML += convertProblemGroup(complaintGroupJson);
+                });
+                complaintGroupTable.addEventListener("click", function(e) { handleProblemGroupTableClick(e, loginCookie)});
+            })
+        } else {
+            response.text().then(value =>{
+                alert("Error occurred during retrieval of complaint groups: " + value);
+            })
+        }
+    })
+}
+
+function handleProblemGroupTableClick(e, loginCookie){
+    let id = e.target.id;
+    if(id == "" || id == "problem-group-table")
+        return;
+    let complaintGroupId = parseInt(id);
+    retrieveQueryResults(loginCookie, complaintGroupId);
+}
+
+function titleCase(string) {
+    var sentence = string.toLowerCase().split(" ");
+    for(var i = 0; i < sentence.length; i++){
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+    return sentence;
+}
+
+function convertProblemGroup(problemGroupJson) {
+    retStr = '<tr><td class="pgroup" id="%">';
+    retStr += titleCase(problemGroupJson.GroupDefinition);
+    retStr += '</td></tr>';
+    retStr = retStr.replace('%', problemGroupJson.Id.toString())
+    return retStr;
+}
+
+function retrieveQueryResults(loginCookie, problemGroupId) {
+    let table = document.getElementById("table-main");
+    let uploadLink = document.getElementById("link");
+    if(queryResultsTableInitialHtml == undefined) {
+        queryResultsTableInitialHtml = table.innerHTML;
+    } else {
+        table.innerHTML = queryResultsTableInitialHtml;
+    }
+    table.style.display = "table";
+    uploadLink.style.display = "block";
+    let cookieStr = decodeURIComponent(document.cookie);
+    let reqBody = undefined;
+    cookieStr.split(';').forEach(function(cookie) {
+        if(cookie.trim().startsWith("OMISA_WEB_COOKIE"))
+            reqBody = cookie.split('=')[1];
+    })
+    if(reqBody == undefined) {
+        window.location.replace("newaquery.html");
+        return;
+    }
+    let bodyObj = JSON.parse(reqBody);
+
+    let reqObj = {
+        Entry: {
+            Make: bodyObj.make.toLowerCase(),
+            Model: bodyObj.model.toLowerCase(),
+            Problem: bodyObj.problem.toLowerCase()
+        },
+        UserId: loginCookie.userId,
+        LoginToken: loginCookie.token,
+        CompanyId: parseInt(bodyObj.companyId),
+        ProblemGroupId: problemGroupId
+    };
+    let fetchInit = {
+        method: "POST",
+        body: JSON.stringify(reqObj)
+    };
+    fetch("https://jcf-ai.com:16384/archive", fetchInit).then(response=>{
+        if(response.status == 200) {
+            response.text().then(value=>{
+                let respJson = JSON.parse(value);
+                respJson.forEach(queryObject => {
+                    let objStr = convertSimilarQueryObject(queryObject);
+                    table.innerHTML += objStr;
+                })
+            })
+        } else {
+            response.text().then(value =>{
+                alert("Error occurred during retrieval of queries from complaint group: " + value);
+            })
+        }
+    })
+}
+
+function convertSimilarQueryObject(queryObject) {
+    let retStr = "<tr>";
+    retStr += "<td>" + titleCase(queryObject.Make) + "</td>";
+    retStr += "<td>" + titleCase(queryObject.Model) + "</td>";
+    retStr += "<td>" + queryObject.Year + "</td>";
+    retStr += "<td>" + queryObject.Problem + "</td>";
+    let similarity = 100-queryObject.Difference + "%";
+    retStr += "<td>" + similarity + "</td></tr>";
+    return retStr;
+}
+
+async function init() {
+    let loginCookie;
+    await IsUserLoggedIn().then(val => {loginCookie = val});
+    if(loginCookie == false) {
+        ConstructRedirectCookie(false, "archiveresults.html");
+        window.location.replace("login.html");
+        alert("User was not logged in, redirecting");
+        return;
+    }
+    retrieveComplaintGroupResults(loginCookie);
+}
+
+window.onload=init;
--- public/newaquery.js
+++ public/newaquery.js
@@ -1,15 +1,74 @@
+function SubButtonClick() {
+    let maketextform = document.getElementById("make-text-form");
+    let modeltextform = document.getElementById("model-text-form");
+    let problemTextForm = document.getElementById("problem-text-form");
+    let companyDropdownForm = document.getElementById("company-dropdown-form");
+    let make = maketextform.value;
+    let model = modeltextform.value;
+    let problem = problemTextForm.value;
+    let companyId = companyDropdownForm.options[companyDropdownForm.selectedIndex].value
+    if(ValidateTextForms(make, model, problem)) {
+        SetCookie(make, model, problem, companyId);
+        window.location.href = "archiveresults.html";
+    }
+}
+
 function Resize() {
     RelocateButton();
     //ResizeFooter();
 }
 
-function ResizeFooter() {
-    let footer = document.getElementsByClassName("footer")[0];
-    footer.style.left = "202px";
-    let footerWidth = window.innerWidth - 397;
-    footer.style.width = footerWidth.toString() + "px";
-    let footerTop = window.innerHeight - footer.offsetHeight;
-    footer.style.top = footerTop.toString() + "px";
+function RetrieveValidCompanies(loginCookie) {
+    let reqObj = {
+        UserId: loginCookie.userId,
+        LoginToken: loginCookie.token
+    };
+    let fetchInit = {
+        method: "POST",
+        body: JSON.stringify(reqObj)
+    };
+    fetch("https://jcf-ai.com:16384/company/list", fetchInit).then(response=>{
+        response.text().then(value => {
+            if(response.status == 200) {
+                let companyDropdownForm = document.getElementById("company-dropdown-form");
+                let respObj = JSON.parse(value);
+                respObj.forEach((companyIdObj, index) => {
+                    companyDropdownForm.innerHTML += convertCompanyIdObject(companyIdObj, index == respObj.length);
+                })
+            } else{
+                alert("Retrieving valid companies failed: " + value);
+            }
+
+        })
+    })
+}
+
+function convertCompanyIdObject(companyIdObj, isLast) {
+    let retStr = '<option value="' + companyIdObj.Id + '"';
+    if(isLast)
+        retStr += ' selected';
+    retStr += '>' + companyIdObj.LegalName + "</option>";
+    return retStr;
+}
+
+function DeleteCookie() {
+    let d = new Date(1970, 0, 1);
+    document.cookie = "OMISA_WEB_COOKIE=null;expires=" + d.toUTCString() + ";"
+}
+
+function ValidateTextForms(make, model, problem) {
+    return (make !== "") && (model !== "") && (problem != "");
+}
+
+function SetCookie(make, model, problem, companyId) {
+    let d = new Date();
+    d.setTime(d.getTime() + (7 * 1000 * 60 * 60 * 24));
+    let cookieStr = "OMISA_WEB_COOKIE={";
+    cookieStr += '"make":"' + make + '",';
+    cookieStr += '"model":"' + model + '",';
+    cookieStr += '"companyId":"' + companyId + '",';
+    cookieStr += '"problem":"' + problem + '"}';
+    document.cookie = cookieStr + ";expires="+d.toUTCString()+";path=/;";
 }
 
 function RelocateButton() {
@@ -19,7 +78,35 @@ function RelocateButton() {
     button.style.left = buttonPosition.toString() + "px";
 }
 
-function init() {
+async function init() {
+    let loginCookie;
+    await IsUserLoggedIn().then(val => {loginCookie = val});
+    if(loginCookie == false) {
+        ConstructRedirectCookie(false, "newaquery.html");
+        window.location.replace("login.html");
+        alert("User was not logged in, redirecting");
+        return;
+    }
+    let button = document.getElementById("submitbutton");
+    button.onclick = SubButtonClick;
+    let maketextform = document.getElementById("make-text-form");
+    let modeltextform = document.getElementById("model-text-form");
+    let problemtextform = document.getElementById("problem-text-form");
+    let companyDropdownForm = document.getElementById("company-dropdown-form");
+    let yearTextForm = document.getElementById("year-text-form");
+    let overrideSubmit = function(e) {
+        if(e.keyCode == 13) {
+            e.preventDefault();
+            button.click();
+        }
+    }
+    maketextform.addEventListener("keydown", overrideSubmit);
+    modeltextform.addEventListener("keydown", overrideSubmit);
+    problemtextform.addEventListener("keydown", overrideSubmit);
+    companyDropdownForm.addEventListener("keydown", overrideSubmit);
+    yearTextForm.addEventListener("keydown", overrideSubmit);
+    RetrieveValidCompanies(loginCookie);
+
     window.addEventListener("resize", Resize);
     Resize();
 }
